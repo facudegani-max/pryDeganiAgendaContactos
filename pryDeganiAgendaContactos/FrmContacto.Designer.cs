909aeca18c26ded9f0e78238851d0035221ae30b
facudegani-max <facudegani@gmail.com>
2025-09-05 12:55:02 -0300

    color

--- pryDeganiAgendaContactos/FrmContacto.Designer.cs
+++ pryDeganiAgendaContactos/FrmContacto.Designer.cs
@@ -38,103 +38,115 @@
             panel1 = new Panel();
             panel2 = new Panel();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // lblContacto
             // 
             lblContacto.AutoSize = true;
-            lblContacto.Location = new Point(13, 8);
+            lblContacto.Location = new Point(15, 9);
             lblContacto.Name = "lblContacto";
-            lblContacto.Size = new Size(56, 15);
+            lblContacto.Size = new Size(63, 17);
             lblContacto.TabIndex = 0;
             lblContacto.Text = "Contacto";
             // 
             // lblTelefono
             // 
             lblTelefono.AutoSize = true;
-            lblTelefono.Location = new Point(132, 78);
+            lblTelefono.BackColor = SystemColors.GradientActiveCaption;
+            lblTelefono.ForeColor = SystemColors.ActiveCaptionText;
+            lblTelefono.Location = new Point(16, 9);
             lblTelefono.Name = "lblTelefono";
-            lblTelefono.Size = new Size(52, 15);
+            lblTelefono.Size = new Size(62, 17);
             lblTelefono.TabIndex = 1;
             lblTelefono.Text = "Telefono";
             // 
             // txtContacto
             // 
-            txtContacto.Location = new Point(228, 35);
+            txtContacto.Location = new Point(147, 35);
             txtContacto.Name = "txtContacto";
-            txtContacto.Size = new Size(100, 23);
+            txtContacto.Size = new Size(114, 25);
             txtContacto.TabIndex = 2;
             txtContacto.TextChanged += txtContacto_TextChanged;
             // 
             // mtbTelefono
             // 
-            mtbTelefono.Location = new Point(228, 78);
+            mtbTelefono.Location = new Point(147, 82);
             mtbTelefono.Mask = "(999)000-0000";
             mtbTelefono.Name = "mtbTelefono";
-            mtbTelefono.Size = new Size(134, 23);
+            mtbTelefono.Size = new Size(153, 25);
             mtbTelefono.TabIndex = 3;
             // 
             // btnCargar
             // 
-            btnCargar.Location = new Point(278, 135);
+            btnCargar.BackColor = Color.RosyBrown;
+            btnCargar.Location = new Point(254, 133);
             btnCargar.Name = "btnCargar";
-            btnCargar.Size = new Size(96, 36);
+            btnCargar.Size = new Size(110, 41);
             btnCargar.TabIndex = 4;
             btnCargar.Text = "Cargar";
-            btnCargar.UseVisualStyleBackColor = true;
+            btnCargar.UseVisualStyleBackColor = false;
+            btnCargar.Click += btnCargar_Click;
             // 
             // btnSalir
             // 
-            btnSalir.Location = new Point(165, 135);
+            btnSalir.BackColor = Color.RosyBrown;
+            btnSalir.Location = new Point(114, 133);
             btnSalir.Name = "btnSalir";
-            btnSalir.Size = new Size(107, 36);
+            btnSalir.Size = new Size(122, 41);
             btnSalir.TabIndex = 5;
             btnSalir.Text = "Salir";
-            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.UseVisualStyleBackColor = false;
             // 
             // lstContacto_Telefono
             // 
             lstContacto_Telefono.FormattingEnabled = true;
-            lstContacto_Telefono.ItemHeight = 15;
-            lstContacto_Telefono.Location = new Point(12, 229);
+            lstContacto_Telefono.ItemHeight = 17;
+            lstContacto_Telefono.Location = new Point(14, 207);
             lstContacto_Telefono.Name = "lstContacto_Telefono";
-            lstContacto_Telefono.Size = new Size(398, 94);
+            lstContacto_Telefono.Size = new Size(350, 106);
             lstContacto_Telefono.TabIndex = 6;
             // 
             // panel1
             // 
+            panel1.BackColor = SystemColors.GradientActiveCaption;
             panel1.Controls.Add(lblContacto);
-            panel1.Location = new Point(115, 30);
+            panel1.Location = new Point(23, 29);
             panel1.Name = "panel1";
-            panel1.Size = new Size(85, 32);
+            panel1.Size = new Size(97, 36);
             panel1.TabIndex = 7;
             // 
             // panel2
             // 
+            panel2.BackColor = SystemColors.GradientActiveCaption;
+            panel2.Controls.Add(lblTelefono);
             panel2.ForeColor = SystemColors.ActiveCaption;
-            panel2.Location = new Point(115, 71);
+            panel2.Location = new Point(23, 76);
             panel2.Name = "panel2";
-            panel2.Size = new Size(85, 30);
+            panel2.Size = new Size(97, 34);
             panel2.TabIndex = 8;
             panel2.Paint += panel2_Paint;
             // 
             // FrmContacto
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(440, 375);
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(376, 334);
             Controls.Add(lstContacto_Telefono);
             Controls.Add(btnSalir);
             Controls.Add(btnCargar);
             Controls.Add(mtbTelefono);
             Controls.Add(txtContacto);
-            Controls.Add(lblTelefono);
             Controls.Add(panel1);
             Controls.Add(panel2);
+            Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "FrmContacto";
             Text = "Agenda ";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
--- pryDeganiAgendaContactos/FrmContacto.cs
+++ pryDeganiAgendaContactos/FrmContacto.cs
@@ -16,5 +16,10 @@ namespace pryDeganiAgendaContactos
         {
 
         }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
